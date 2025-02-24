28f5266930d86e37ef7475588c4e957dff274676
GuilhermeCerabando <cerabando@gmail.com>
2025-02-24 11:04:56 -0300

    Finaliza programa com as 5 frutas favoritas

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿Console.WriteLine("################################");
+Console.WriteLine("####### Frutas Preferidas ######");
+Console.WriteLine("################################");
+
+Console.WriteLine();
+
+Console.ResetColor();
+
+Console.Write("1. ");
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.Write("Uva");
+Console.ResetColor();
+
+Console.WriteLine();
+
+Console.Write("2. ");
+Console.ForegroundColor = ConsoleColor.Green;
+Console.Write("Laranja");
+Console.ResetColor();
+
+Console.WriteLine();
+
+Console.Write("3. ");
+Console.ForegroundColor = ConsoleColor.Red;
+Console.Write("Morango");
+Console.ResetColor();
+
+Console.WriteLine();
+
+Console.Write("4. ");
+Console.ForegroundColor = ConsoleColor.DarkYellow;
+Console.Write("Mexirica");
+Console.ResetColor();
+
+Console.WriteLine();
+
+Console.Write("5. ");
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.Write("Abacaxi");
+Console.ResetColor();
+
